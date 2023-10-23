1ce62234509727bda0111a416fe6d217c6f003eb
Ing-ed <emidovichi@gmail.com>
2023-10-23 16:59:50 -0300

    actualizados los metodos de carts

--- src/routes/carrito.router.js
+++ src/routes/carrito.router.js
@@ -25,7 +25,7 @@ router.post("/",async (req,res) =>{
         // })
     } catch (error){
         res.send({result:"error",error:error});
-        console.log("Error")
+        console.log("Error", error)
     }
     // console.log("body",req.body);
     // manager.addProductToCart(req.body)
@@ -36,30 +36,51 @@ router.post("/",async (req,res) =>{
     // })
 })
 
-router.put("/:cid/product/:pid",(req,res)=>{
+router.put("/:cid/product/:pid",async (req,res)=>{
     const {cid,pid} = req.params
     console.log("params",cid,pid)
-    manager.updateProductFromCart({cartId:+cid,prodId:+pid,quantity:req.body.quantity})
-    .then((r) => {
-        res.send({message:r});
-    }) .catch((e) =>{
-        res.send({message:e})
-    })
+    try{
+        let carts = await cartModel.find()
+        let cartIndex = carts.findIndex((item) => item.cartId === +cid)
+        console.log(carts[cartIndex]);
+        let prodIndex = carts[cartIndex].products.findIndex((item) => item.prodId === +pid);
+        let prod = []
+        if(prodIndex >= 0){
+            prod = {prodId:+pid,quantity:+carts[cartIndex].products[prodIndex].quantity + +req.body.quantity}; //es el producto a actualizar
+        } else {
+            prod = carts[cartIndex].products
+            prod.push({prodId:+pid,quantity:+req.body.quantity})
+        }
+        console.log(prod)
+        let result = await cartModel.updateOne({_id:carts[cartIndex]._id},{products:prod})
+        res.send({result:"sucess",payload:result})
+        // console.log(result)
+    } catch (error){
+        console.log("error",error)
+    }
+
+    // manager.updateProductFromCart({cartId:+cid,prodId:+pid,quantity:req.body.quantity})
+    // .then((r) => {
+    //     res.send({message:r});
+    // }) .catch((e) =>{
+    //     res.send({message:e})
+    // })
 })
 
 router.get("/:cid",async (req,res) =>{
     const {cid} = req.params;
     try{
         let carts = await cartModel.find();
-        let index = carts.findIndex((item) => item.id === cid)
+        console.log(carts, "Carritos")
+        let index = carts.findIndex((item) => item.cartId === +cid)
         if(index <0 ){
             res.send({result:"error",cause:"item no encontrado"});
         } else{
             res.send({result:"sucess",payload:carts[index]})
         }
-        console.log(carts)
+        // console.log(carts)
     } catch (error){
-        console.log(error)
+        console.log("error", error)
     }
     // manager.getCartById({cartId:+cid})
     // .then((r) => {
