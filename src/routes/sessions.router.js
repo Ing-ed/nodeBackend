cabfaa6d13e1a680e844cc4522676a1d0cb23742
Ing-ed <emidovichi@gmail.com>
2023-11-24 16:23:22 -0300

    Funcionando con cookies

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -65,7 +65,7 @@ router.post("/login",async (req,res) =>{
         const {firstName, lastName,rol} = exist
         const token = TokenGen({firstName,lastName,email,rol})
         console.log(token)
-        res.cookie("auth",token).redirect(`/productos/${exist._id}`)
+        res.cookie("auth",token,{httpOnly:true}).redirect(`/productos/${exist._id}`)
         // res.json({message:"Token",token:token})
         // res.redirect(`/productos/${exist._id}`)
     }catch (error){
